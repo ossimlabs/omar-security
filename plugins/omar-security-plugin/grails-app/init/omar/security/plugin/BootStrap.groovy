de3ef03656439062969985c97e72ee2529763264
Dylan-Cannisi <Dylan.Cannisi@digitalglobe.com>
2019-06-05 11:39:13 -0400

    Added default values in config file and updates default user upon deploy.

--- plugins/omar-security-plugin/grails-app/init/omar/security/plugin/BootStrap.groovy
+++ plugins/omar-security-plugin/grails-app/init/omar/security/plugin/BootStrap.groovy
@@ -7,11 +7,15 @@ class BootStrap {
 
 
     def init = { servletContext ->
-        def defaultUsername = grailsApplication.config.defaultUsername
+        def defaultUsername = grailsApplication.config.defaultPreferences.defaultUsername
         def defaultUser = User.findByUsername( defaultUsername )
-        if ( !defaultUser ) {
-            securityService.addNewUser( defaultUsername )
+        // Todo: Add code to look up the config and init defaultUser to those preferences
+        // additionally if defaultUser exist change the preferences to resemble the current
+        // state of the config if things had changed.
+        if ( defaultUser ) {
+            defaultUser.delete( flush: true )
         }
+        securityService.addNewUser( defaultUsername )
     }
     def destroy = {
     }
