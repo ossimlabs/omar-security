4db36f5493df99a9c086d560279d204c21d29af0
sbortman <scott.bortman@gmail.com>
2019-10-24 08:40:59 -0400

    optimizing js deps

--- plugins/omar-security-plugin/grails-app/assets/javascripts/preferences-bundle.js
+++ plugins/omar-security-plugin/grails-app/assets/javascripts/preferences-bundle.js
@@ -1,5 +1,5 @@
-//= require webjars/jquery/2.2.0/jquery.min
+//= require webjars/jquery/3.4.1/jquery.min
 //= require webjars/bootstrap/3.3.5/js/bootstrap.min
-//= require webjars/momentjs/2.11.0/moment.js
-//= require webjars/Eonasdan-bootstrap-datetimepicker/4.15.35/js/bootstrap-datetimepicker.min
+//= require webjars/momentjs/2.24.0/moment.js 
+//= require webjars/Eonasdan-bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min
 //= require preferences
