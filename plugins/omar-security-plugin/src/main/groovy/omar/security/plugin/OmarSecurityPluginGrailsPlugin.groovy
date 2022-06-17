e794959c1a0fbec743419f7abb40c73ca48970db
scott bortman <scott.bortman@maxar.com>
2022-06-17 14:04:45 -0400

    initial import

--- plugins/omar-security-plugin/src/main/groovy/omar/security/plugin/OmarSecurityPluginGrailsPlugin.groovy
+++ plugins/omar-security-plugin/src/main/groovy/omar/security/plugin/OmarSecurityPluginGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class OmarSecurityPluginGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.10 > *"
+    def grailsVersion = "5.1.8 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
